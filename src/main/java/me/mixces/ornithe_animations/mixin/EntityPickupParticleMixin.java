9169ddcc86ea002673ad741cd487736070fa378a
Mixces <aar.hcms@gmail.com>
2024-07-31 21:21:10 -0500

    this is better!

--- src/main/java/me/mixces/ornithe_animations/mixin/EntityPickupParticleMixin.java
+++ src/main/java/me/mixces/ornithe_animations/mixin/EntityPickupParticleMixin.java
@@ -1,30 +1,39 @@
 package me.mixces.ornithe_animations.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.vertex.BufferBuilder;
 import me.mixces.ornithe_animations.util.GlHelper;
 import net.minecraft.client.entity.particle.EntityPickupParticle;
-import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityPickupParticle.class)
 public abstract class EntityPickupParticleMixin {
 
-	@WrapOperation(
+	@Inject(
 		method = "render",
 		at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/client/render/entity/EntityRenderDispatcher;render(Lnet/minecraft/entity/Entity;DDDFF)Z"
 		)
 	)
-	private boolean ornitheAnimations$pickupPosition(EntityRenderDispatcher instance, Entity entity, double dx, double dy, double dz, float yaw, float tickDelta, Operation<Boolean> original) {
+	private void ornitheAnimations$pushPosition(BufferBuilder bufferBuilder, Entity camera, float tickDelta, float dx, float dy, float dz, float forwards, float sideways, CallbackInfo ci) {
 		GlStateManager.pushMatrix();
 		GlHelper.getBuilder().translate(0.0F, 0.5F, 0.0F);
-		original.call(instance, entity, dx, dy, dz, yaw, tickDelta);
+	}
+
+	@Inject(
+		method = "render",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/render/entity/EntityRenderDispatcher;render(Lnet/minecraft/entity/Entity;DDDFF)Z",
+			shift = At.Shift.AFTER
+		)
+	)
+	private void ornitheAnimations$popPosition(BufferBuilder bufferBuilder, Entity camera, float tickDelta, float dx, float dy, float dz, float forwards, float sideways, CallbackInfo ci) {
 		GlStateManager.popMatrix();
-		return true;
 	}
 }
